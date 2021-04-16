09cea2da4c3a4058466ab3fb3eedbec85ae9351f
Szymon-Gosk <szymongosk767@gmail.com>
2021-04-16 22:25:15 +0200

    [DEV 1.2]

--- dinners-service/src/main/java/gosk/szymon/controllers/PersonsController.java
+++ dinners-service/src/main/java/gosk/szymon/controllers/PersonsController.java
@@ -3,17 +3,17 @@ package gosk.szymon.controllers;
 import gosk.szymon.models.Person;
 import gosk.szymon.repositories.PersonRepository;
 import org.jetbrains.annotations.Contract;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class PersonsController {
 
     private final PersonRepository repository;
